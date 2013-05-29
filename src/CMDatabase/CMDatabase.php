47274f324002f30897de54114b991f29d00b3e78
Sebastian Hultstrand <sebastian@atlan.se>
2013-05-29 17:14:10 +0200

    Added a database interaction class

---
+++ src/CMDatabase/CMDatabase.php
@@ -0,0 +1,77 @@
+<?php
+/**
+* Database wrapper, provides a database API for the framework but hides details of implementation.
+*
+* @package StormFrame
+*/
+class CMDatabase {
+
+   	/**
+    * Members
+    */
+  	private $dbh = null;
+  	private $stmt = null;
+  	private static $numQueries = 0;
+  	private static $queries = array();
+
+  	/**
+   	* Constructor
+   	*/
+  	public function __construct($dsn, $username = null, $password = null, $driver_options = null) {
+    	$this->dbh = new PDO($dsn, $username, $password, $driver_options);
+    	$this->dbh->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  	}
+
+  	/**
+   	* Set an attribute on the database
+   	*/
+  	public function SetAttribute($attribute, $value) {
+    	return $this->dbh->setAttribute($attribute, $value);
+  	}
+
+  	/**
+   	* Getters
+   	*/
+  	public function GetNumQueries() {
+  		 return self::$numQueries; 
+	}
+	
+  	public function GetQueries() {
+  		 return self::$queries; 
+	}
+
+   	/**
+    * Execute a select-query with arguments and return the resultset.
+    */
+  	public function ExecuteSelectQueryAndFetchAll($query, $params=array()){
+    	$this->stmt = $this->dbh->prepare($query);
+    	self::$queries[] = $query;
+    	self::$numQueries++;
+    	$this->stmt->execute($params);
+    	return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+  	}
+
+  	/**
+   	* Execute a SQL-query and ignore the resultset.
+   	*/
+  	public function ExecuteQuery($query, $params = array()) {
+    	$this->stmt = $this->dbh->prepare($query);
+    	self::$queries[] = $query;
+    	self::$numQueries++;
+    	return $this->stmt->execute($params);
+  	}
+
+  	/**
+   	* Return last insert id.
+   	*/
+  	public function LastInsertId() {
+    	return $this->dbh->lastInsertid();
+  	}
+
+  	/**
+   	* Return rows affected of last INSERT, UPDATE, DELETE
+   	*/
+  	public function RowCount() {
+    	return is_null($this->stmt) ? $this->stmt : $this->stmt->rowCount();
+  	}
+}
