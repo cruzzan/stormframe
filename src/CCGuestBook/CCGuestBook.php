313cb7caa3ee019d179119323e9f36a6c871a48e
Sebastian Hultstrand <sebastian@atlan.se>
2013-05-30 00:35:04 +0200

    Sparated PHP from SQL code in guestbook with an SQL interface class and method.

--- src/CCGuestBook/CCGuestBook.php
+++ src/CCGuestBook/CCGuestBook.php
@@ -4,7 +4,7 @@
 *
 * @package StormFrame
 */
-class CCGuestbook extends CObject implements IController {
+class CCGuestbook extends CObject implements IController, IHasSQL {
 
 	private $pageTitle = 'StormFrame Guestbook Example';
   	private $pageHeader = '<h1>Guestbook Example</h1><p>Showing off how to implement a guestbook in StormFrame.</p>';
@@ -18,6 +18,23 @@ class CCGuestbook extends CObject implements IController {
   	public function __construct() {
     	parent::__construct();
   	}
+	
+	/**
+    * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
+    *
+    * @param string $key the string that is the key of the wanted SQL-entry in the array.
+    */
+  	public static function SQL($key=null) {
+     	$queries = array(
+        	'insert into guestbook'   => 'INSERT INTO Guestbook (content, submitted) VALUES (?, ?);',
+        	'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
+        	'delete all from guestbook'   => 'DELETE FROM Guestbook;',
+     	);
+     	if(!isset($queries[$key])) {
+        	throw new Exception("No such SQL query, key '$key' was not found.");
+      	}
+      	return $queries[$key];
+   	}
  
 
   	/**
@@ -60,7 +77,7 @@ class CCGuestbook extends CObject implements IController {
    	* Adding new guestbook entry
    	*/
    	public function SaveNewEntry($text){
-   		$this->dbh->ExecuteQuery('INSERT INTO Guestbook (content, submitted) VALUES (?, ?)', array($text, date('y-m-d H:i:s')));
+   		$this->dbh->ExecuteQuery(self::SQL('insert into guestbook'), array($text, date('y-m-d H:i:s')));
 		if($this->dbh->rowCount() != 1){
 			echo 'Failed to insert new entry into database';
 		}
@@ -70,7 +87,7 @@ class CCGuestbook extends CObject implements IController {
    	* Adding new guestbook entry
    	*/
    	public function ClearGuestbook(){
-		$this->dbh->ExecuteQuery('DELETE FROM Guestbook;');
+		$this->dbh->ExecuteQuery(self::SQL('delete all from guestbook'));
    	}
 	
 	/**
@@ -79,7 +96,7 @@ class CCGuestbook extends CObject implements IController {
    	public function ReadFromDatabase(){
    		try{
    			$this->dbh->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      		return $this->dbh->ExecuteSelectQueryAndFetchAll('SELECT * FROM Guestbook ORDER BY id DESC;');
+      		return $this->dbh->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
    		}catch(Exception $e){
    			return array();
 		}
